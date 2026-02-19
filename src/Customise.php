f626f9614add94b918509aa2b91ad2cd748435fa
Boris Anthony <964649+BorisAnthony@users.noreply.github.com>
2026-02-19 16:55:56 +0900

    adjust for Stan

--- src/Customise.php
+++ src/Customise.php
@@ -9,7 +9,7 @@ class Customise
     /**
      * Set the symbols for the given measurement units.
      *
-     * @param array $units An associative array where the keys are unit class names and the values are the symbols to set.
+     * @param array<class-string, string> $units An associative array where the keys are unit class names and the values are the symbols to set.
      *              e.g.: ['MeasurementUnit\Length\Meter' => 'METRE', 'MeasurementUnit\Length\Fathom' => 'FATHOM']
      *
      * @throws \InvalidArgumentException If a class does not exist or is not a valid measurement unit.
@@ -19,10 +19,10 @@ class Customise
         foreach ($units as $unitClass => $symbolString) {
             try {
                 $unitClass::setSymbol($symbolString);
-            } catch (\Error $e) {
-                throw new \InvalidArgumentException("Class $unitClass does not exist.");
             } catch (\TypeError $e) {
                 throw new \InvalidArgumentException("Class $unitClass is not a valid measurement unit.");
+            } catch (\Error $e) {
+                throw new \InvalidArgumentException("Class $unitClass does not exist.");
             }
         }
     }
